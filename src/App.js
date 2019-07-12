cc1b0f7d6c87c9051c0f2e2252488f932ab6e825
miscmike <miscmikesyd@gmail.com>
2019-07-12 22:00:18 +1000

    add react-spring and example

--- src/App.js
+++ src/App.js
@@ -2,13 +2,24 @@ import React from "react";
 import logo from "./kickonline_logo.svg";
 import "./App.css";
 import { motion } from "framer-motion";
+import { useSpring, animated } from "react-spring";
 
 function App() {
+  const props = useSpring({
+    opacity: 1,
+    from: { opacity: 0 },
+    delay: 4000,
+    config: { duration: 4000, easing: t => t * t * t }
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Coming soon!</p>
+        <div>
+          <animated.div style={props}>hello</animated.div>
+        </div>
         <motion.div
           animate={{
             x: 0,
@@ -21,7 +32,7 @@ function App() {
               display: "visible"
             }
           }}
-          whileHover={{ rotate: 360, x: 500, backgroundColor: "#FFF" }}
+          whileHover={{ rotate: 360, x: 500, y: -500, backgroundColor: "#FFF" }}
           transition={spring}
         />
       </header>
