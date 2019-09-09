39b3046a9039fc7f6fa5640b15334f45706f7eee
miscmike <miscmikesyd@gmail.com>
2019-09-09 22:23:10 +1000

    test the waters

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Double dance!</p>
+        <p>what do we do</p>
         <div>
           <animated.div style={props}>hello</animated.div>
         </div>
