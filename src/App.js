f6eddeae45eebc69e53813b024f138ecddb317f2
miscmike <miscmikesyd@gmail.com>
2019-09-12 18:17:18 +1000

    remove logo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
-import logo from "./kickonline_logo.svg";
 import "./App.css";
 import { motion } from "framer-motion";
-import { useSpring, animated } from "react-spring";
+// import { useSpring, animated } from "react-spring";
 
 function App() {
-  const props = useSpring({
-    opacity: 1,
-    from: { opacity: 0 },
-    delay: 4000,
-    config: { duration: 4000, easing: t => t * t * t }
-  });
+  // const props = useSpring({
+  //   opacity: 1,
+  //   from: { opacity: 0 },
+  //   delay: 4000,
+  //   config: { duration: 4000, easing: t => t * t * t }
+  // });
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>two lines</p>
         <p>is better than one</p>
-        <div>
+        {/* <div>
           <animated.div style={props}>hello</animated.div>
-        </div>
+        </div> */}
         <motion.div
           animate={{
             x: 0,
