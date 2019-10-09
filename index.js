8becc4e7c9b371182919901b806a78ef32226a89
miscmike <miscmikesyd@gmail.com>
2019-10-10 01:13:12 +1100

    confirm title not an issue

--- index.js
+++ index.js
@@ -1,105 +1,105 @@
-// // Init Zfont plugin and bind to Zdog
-// Zfont.init(Zdog);
+// Init Zfont plugin and bind to Zdog
+Zfont.init(Zdog);
 
-// // Create Zdog Illustration
-// // https://zzz.dog/api#illustration
-// var illo = new Zdog.Illustration({
-//   element: ".zdog-canvas",
-//   dragRotate: true,
-//   rotate: { x: -0.32, y: 0.64, z: 0 },
-//   //   resize: true,
-//   onResize: function(width, height) {
-//     var minSize = Math.min(width, height);
-//     this.zoom = minSize / 420;
-//   }
-// });
+// Create Zdog Illustration
+// https://zzz.dog/api#illustration
+var illo = new Zdog.Illustration({
+  element: ".zdog-canvas",
+  dragRotate: true,
+  rotate: { x: -0.32, y: 0.64, z: 0 },
+  //   resize: true,
+  onResize: function(width, height) {
+    var minSize = Math.min(width, height);
+    this.zoom = minSize / 420;
+  }
+});
 
-// // Create a Font object
-// // You can use any .ttf or .otf font!
-// // https://github.com/jaames/zfont#zdogfont
-// var font = new Zdog.Font({
-//   src: "https://cdn.jsdelivr.net/gh/jaames/zfont/demo/fredokaone.ttf"
-// });
+// Create a Font object
+// You can use any .ttf or .otf font!
+// https://github.com/jaames/zfont#zdogfont
+var font = new Zdog.Font({
+  src: "https://cdn.jsdelivr.net/gh/jaames/zfont/demo/fredokaone.ttf"
+});
 
-// // Create a TextGroup object for the title
-// // https://github.com/jaames/zfont#zdogtextgroup
-// var title = new Zdog.TextGroup({
-//   addTo: illo,
-//   font: font,
-//   value: "Zfont",
-//   fontSize: 60,
-//   translate: { y: -100 },
-//   textAlign: "left",
-//   textBaseline: "middle",
-//   color: "#fff",
-//   fill: true
-// });
+// Create a TextGroup object for the title
+// https://github.com/jaames/zfont#zdogtextgroup
+var title = new Zdog.TextGroup({
+  addTo: illo,
+  font: font,
+  value: "Zfont",
+  fontSize: 60,
+  translate: { y: -100 },
+  textAlign: "left",
+  textBaseline: "middle",
+  color: "#fff",
+  fill: true
+});
 
-// // Duplicate the title to create a shadow effect
-// var titleShadow = title.copyGraph({
-//   translate: { z: -6, y: -100 },
-//   color: "#aab"
-// });
+// Duplicate the title to create a shadow effect
+var titleShadow = title.copyGraph({
+  translate: { z: -6, y: -100 },
+  color: "#aab"
+});
 
-// var titleShadowTwo = title.copyGraph({
-//   translate: { z: -12, y: -100 },
-//   color: "#aab"
-// });
+var titleShadowTwo = title.copyGraph({
+  translate: { z: -12, y: -100 },
+  color: "#aab"
+});
 
-// // Create a TextGroup object for the subtitle
-// // https://github.com/jaames/zfont#zdogtextgroup
-// var sub = new Zdog.TextGroup({
-//   addTo: illo,
-//   font: font,
-//   // Pass an array as the text value for multiline text:
-//   value: ["FIRST", "DANCE"],
-//   fontSize: 128,
-//   textAlign: "center",
-//   textBaseline: "middle",
-//   color: "#ea0af1",
-//   fill: true
-// });
+// Create a TextGroup object for the subtitle
+// https://github.com/jaames/zfont#zdogtextgroup
+var sub = new Zdog.TextGroup({
+  addTo: illo,
+  font: font,
+  // Pass an array as the text value for multiline text:
+  value: ["FIRST", "DANCE"],
+  fontSize: 128,
+  textAlign: "center",
+  textBaseline: "middle",
+  color: "#ea0af1",
+  fill: true
+});
 
-// // Duplicate the subtitle to create a shadow effect
-// var subShadow = sub.copyGraph({
-//   translate: { z: 10 },
-//   color: "#FFF"
-// });
+// Duplicate the subtitle to create a shadow effect
+var subShadow = sub.copyGraph({
+  translate: { z: 10 },
+  color: "#FFF"
+});
 
-// // Settings for the wave animation
-// var t = 0;
-// var tStep = 5;
-// var amplitude = 1;
-// var frequency = 160;
+// Settings for the wave animation
+var t = 0;
+var tStep = 5;
+var amplitude = 1;
+var frequency = 160;
 
-// // Wave function
-// // This loops through every shape in a TextGroup and modifies its position according to a sine wave
-// function wave(group) {
-//   group.children.forEach(shape => {
-//     var x = shape.translate.x + t;
-//     shape.translate.y += amplitude * Math.sin(x / frequency);
-//   });
-// }
+// Wave function
+// This loops through every shape in a TextGroup and modifies its position according to a sine wave
+function wave(group) {
+  group.children.forEach(shape => {
+    var x = shape.translate.x + t;
+    shape.translate.y += amplitude * Math.sin(x / frequency);
+  });
+}
 
-// // Animation loop
-// function animate() {
-//   wave(title);
-//   wave(titleShadow);
-//   wave(sub);
-//   wave(subShadow);
+// Animation loop
+function animate() {
+  wave(title);
+  wave(titleShadow);
+  wave(sub);
+  wave(subShadow);
 
-//   //   sub.value[0] = sub.value[0] + "a";
+  //   sub.value[0] = sub.value[0] + "a";
 
-//   t += tStep;
-//   //   illo.translate.x += 1;
-//   //   illo.transform.opacity +
-//   //   illo.translate.x += 0.3 * Math.sign(Math.random() - 0.5);
-//   illo.updateRenderGraph();
-//   requestAnimationFrame(animate);
-// }
-// // illo.translate.x += -600;
+  t += tStep;
+  //   illo.translate.x += 1;
+  //   illo.transform.opacity +
+  //   illo.translate.x += 0.3 * Math.sign(Math.random() - 0.5);
+  illo.updateRenderGraph();
+  requestAnimationFrame(animate);
+}
+// illo.translate.x += -600;
 
-// animate();
+animate();
 
 window.onload = function(e) {
   let clock = document.querySelector("#clock"),
