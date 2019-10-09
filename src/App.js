9f216ff5adfdbf48cc9cafb181b35f69be2261c5
miscmike <miscmikesyd@gmail.com>
2019-10-09 23:41:34 +1100

    get it all out there

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./App.css";
 import { motion } from "framer-motion";
 import QRCode from "qrcode.react";
+import Zfont from "zfont";
+import Zdog from "zdog";
 // import { useSpring, animated } from "react-spring";
+
+import {
+  Illustration,
+  Ellipse,
+  Shape,
+  RoundedRect,
+  useRender
+} from "react-zdog";
+// New react-spring target, for native animation outside of React
+import { a, useSpring } from "react-spring/zdog";
+
+/** --- Basic, re-usable shapes -------------------------- */
+const TAU = Math.PI * 2;
+
+/** --- Assembly ----------------------------------------- */
+function Guy() {
+  // Change motion every second
+  // const [up, setUp] = useState(true);
+  // useEffect(
+  //   () => void setInterval(() => setUp(previous => !previous), 450),
+  //   []
+  // );
+  // // Turn static values into animated values
+  // const { rotation, color, size } = useSpring({
+  //   size: up ? 1.2 : 0.2,
+  //   color: up ? "#EA0" : "tomato",
+  //   rotation: up ? 0 : Math.PI
+  // });
+  // // useRender allows us to hook into the render-loop
+  // const ref = useRef();
+  // let t = 0;
+  // useRender(() => (ref.current.rotate.y = Math.cos((t += 0.1) / TAU)));
+  return (
+    // <Shape ref={ref} path={[{ x: -3 }, { x: 3 }]} stroke={4} color="#747B9E">
+    <Shape
+      path={[{ x: -1.5 }, { x: 1.5 }]}
+      translate={{ y: -6 }}
+      stroke={1}
+      color="#E1E5EE"
+    ></Shape>
+    // {/* </a.Anchor> */}
+    // </Shape>
+  );
+}
 
 function App() {
   // const props = useSpring({
@@ -15,6 +61,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <Illustration zoom={8}>
+          <Shape stroke={20} color="lightblue" rotate={{ x: Math.PI }} />
+        </Illustration> */}
+        <Illustration zoom={8}>
+          <Ellipse
+            diameter={20}
+            rotate={{ x: -TAU / 3 }}
+            translate={{ y: 15, z: -100 }}
+            stroke={4}
+            color="#373740"
+            fill
+          />
+          <Guy />
+        </Illustration>
         {/* <motion.div
           animate={{
             x: 0,
@@ -51,7 +111,7 @@ function App() {
             </label>
           </p>
           <p>
-            <button type="submit">party time</button>
+            <button type="submit">tell me more</button>
           </p>
         </form>
       </header>
@@ -59,10 +119,10 @@ function App() {
   );
 }
 
-const spring = {
-  type: "spring",
-  damping: 200,
-  stiffness: 20
-};
+// const spring = {
+//   type: "spring",
+//   damping: 200,
+//   stiffness: 20
+// };
 
 export default App;
