fb1ea71a535969bd0ea360313095b37a69ed648f
miscmike <miscmikesyd@gmail.com>
2019-11-05 01:22:34 +1100

    add fade on scroll

---
+++ scroll.js
@@ -0,0 +1,53 @@
+// $(".zdog").hide();
+
+// $(window).scroll(function() {
+//   if ($(this).scrollTop() > 250) {
+//     if ($(this).scrollTop() > 1250)
+//       $(".zdog")
+//         .stop()
+//         .fadeOut();
+//     else
+//       $(".zdog")
+//         .stop()
+//         .fadeIn();
+//   }
+// });
+
+let canvasArray = document.querySelectorAll(".zdog");
+console.log(canvasArray);
+window.addEventListener(
+  "scroll",
+  function(event) {
+    canvasArray.forEach(function(canvas) {
+      if (isInViewport(canvas)) {
+        canvas.classList.add("active");
+        canvas.classList.remove("inactive");
+      } else {
+        canvas.classList.add("inactive");
+        canvas.classList.remove("active");
+      }
+    });
+  },
+  false
+);
+
+var isInViewport = function(elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+    bounding.top >= 0 &&
+    bounding.left >= 0 &&
+    bounding.bottom <= window.innerHeight &&
+    bounding.right <= window.innerWidth
+  );
+};
+// var isInViewport = function(elem) {
+//   var bounding = elem.getBoundingClientRect();
+//   return (
+//     bounding.top >= 0 &&
+//     bounding.left >= 0 &&
+//     bounding.bottom <=
+//       (window.innerHeight || document.documentElement.clientHeight) &&
+//     bounding.right <=
+//       (window.innerWidth || document.documentElement.clientWidth)
+//   );
+// };
